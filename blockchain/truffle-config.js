1064a115ace32d7e10042f2b954af799481baefd
Felipe Louzas <felipe.louzas@gmail.com>
2022-12-11 11:36:47 -0300

    configure truffle for alfajores deploy

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -41,10 +41,10 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -69,6 +69,12 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "5777",       // Any network (default: none)
     },
+
+    testnet_celo: {
+      provider: () => new HDWalletProvider(MNEMONIC, "https://alfajores-forno.celo-testnet.org"),
+      network_id: 44787,
+      gas: 20000000
+    }
     //
     // An additional network, but with some advanced options…
     // advanced: {
