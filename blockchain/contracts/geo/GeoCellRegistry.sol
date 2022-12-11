23ca3fc6bf28add7e471c31a2664935796440e7d
Felipe Louzas <felipe.louzas@gmail.com>
2022-12-11 06:50:32 -0300

    WIP: contratos validacao registro

--- blockchain/contracts/geo/GeoCellRegistry.sol
+++ blockchain/contracts/geo/GeoCellRegistry.sol
@@ -37,48 +37,68 @@ abstract contract GeoCellRegistry {
 
     /**
      * Cell data structure:
-     * - [prefix length (6 bits)][prefix][cell length (6 bits)][cell][cell length (6 bits)][cell]...
+     * == 89C2598B58C = 100 01001110000100101100110001 011010110001 10000000000000000000000
+     * == 89C2598C97  = 100 01001110000100101100110001 1001001011 1000000000000000000000000
+     * - remove lsb + sentinel
+     * - add zero bit to msb
+     * == 89C2598B58C = 0100 01001110000100101100110001 011010110001
+     * == 89C2598C97  = 0100 01001110000100101100110001 1001001011
+     * - encode prefix and cells into byte array, prepend bit length in first byte
+     * == [prefix len (30)][0100 0100][11100001][00101100][110001--][cell0 len (12)][01101011][0001----][cell1 len (10)][10010010][11------]
      */
     function _claimLand(uint256 _propId, bytes memory _cellData) internal {
         require(_propId > 0, "ID da propriedade deve ser maior que 0");
 
-        uint8 prefixLen = _read8(_cellData, 0, 6);
-        require(
-            prefixLen >= 3 && prefixLen % 2 == 1,
-            "Tamanho do prefixo invalido"
-        );
-        uint64 prefix = _read64(_cellData, 6, prefixLen);
+        uint8 prefixBits = uint8(_cellData[0]);
+        require(prefixBits % 2 == 0, "Tamanho do prefixo invalido");
 
-        GeoCellNode storage commonNode = _getCommonNode(prefixLen, prefix);
+        if (prefixBits == 0) {
+            uint256 pos = 0;
 
-        uint256 dataIdx = prefixLen + 6;
-        while (dataIdx < _cellData.length) {
-            uint8 blockLen = _read8(_cellData, dataIdx, 6);
-            dataIdx += 6;
+            while (pos < _cellData.length) {
+                uint8 blockLen = uint8(_cellData[pos]);
+                pos += 1;
 
-            require(
-                blockLen > 0 && blockLen % 2 == 0,
-                "Tamanho do bloco invalido"
+                require(
+                    blockLen > 0 && blockLen % 2 == 0,
+                    "Tamanho do bloco invalido"
+                );
+
+                _claimCell(_propId, pos, blockLen, _cellData);
+                pos += ((blockLen - 1) / 8) + 1;
+            }
+        } else {
+            GeoCellNode storage commonNode = _getCommonNode(
+                prefixBits,
+                _cellData
             );
-            uint64 cellId = _read64(_cellData, dataIdx, blockLen);
-            dataIdx += blockLen;
 
-            _claimCell(_propId, commonNode, blockLen, cellId);
+            uint256 pos = ((prefixBits - 1) / 8) + 2;
+            while (pos < _cellData.length) {
+                uint8 blockLen = uint8(_cellData[pos]);
+                pos += 1;
+
+                require(
+                    blockLen > 0 && blockLen % 2 == 0,
+                    "Tamanho do bloco invalido"
+                );
+
+                _claimCell(_propId, commonNode, pos, blockLen, 0, _cellData);
+                pos += ((blockLen - 1) / 8) + 1;
+            }
         }
     }
 
-    function _getCommonNode(uint8 prefixLen, uint64 prefix)
+    function _getCommonNode(uint8 prefixBits, bytes memory _cellData)
         internal
         returns (GeoCellNode storage)
     {
-        uint8 commonLen = prefixLen - 3;
-        uint8 faceId = uint8(prefix >> commonLen);
-
+        uint8 faceId = (uint8(_cellData[1]) >> 4) & 0x7;
         GeoCellNode storage commonNode = _registeredLand[faceId]; // set commonNode to face cell
         // it´s not possible to register the whole face cell, so no need for propId check;
 
-        for (uint8 idx = 0; idx < commonLen; idx += 2) {
-            uint8 quadKey = uint8((prefix >> (commonLen - idx - 2)) & 0x3);
+        for (uint8 bit = 4; bit < prefixBits; bit += 2) {
+            uint8 quadKey = _getQuadKey(_cellData, 1, bit);
 
             if (commonNode.cells[quadKey]) {
                 // mapping for this quadKey already exists
@@ -100,14 +120,27 @@ abstract contract GeoCellRegistry {
 
     function _claimCell(
         uint256 _propId,
-        GeoCellNode storage _commonNode,
+        uint256 _pos,
         uint8 _blockLen,
-        uint64 _cellId
+        bytes memory _cellData
+    ) internal {
+        uint8 faceId = (uint8(_cellData[_pos]) >> 4) & 0x7;
+        GeoCellNode storage commonNode = _registeredLand[faceId];
+        _claimCell(_propId, commonNode, _pos, _blockLen, 4, _cellData);
+    }
+
+    function _claimCell(
+        uint256 _propId,
+        GeoCellNode storage _commonNode,
+        uint256 _pos,
+        uint8 _blockLen,
+        uint8 _offset,
+        bytes memory _cellData
     ) internal {
         GeoCellNode storage currentNode = _commonNode;
 
-        for (uint8 idx = 0; idx < _blockLen; idx += 2) {
-            uint8 quadKey = uint8((_cellId >> (_blockLen - idx - 2)) & 0x3);
+        for (uint8 bit = _offset; bit < _blockLen; bit += 2) {
+            uint8 quadKey = _getQuadKey(_cellData, _pos, bit);
 
             if (currentNode.cells[quadKey]) {
                 // mapping for this quadKey already exists
@@ -125,7 +158,7 @@ abstract contract GeoCellRegistry {
         }
 
         // currentNode can be registered if no children nodes exist
-        for (uint8 quadKey = 0; quadKey < 3; quadKey++) {
+        for (uint8 quadKey = 0; quadKey < 4; quadKey++) {
             require(
                 currentNode.cells[quadKey] == false,
                 "Registro sobrepoe area registrada"
@@ -135,51 +168,54 @@ abstract contract GeoCellRegistry {
         currentNode.propId = _propId;
     }
 
-    function _read8(
-        bytes memory _data,
-        uint256 _start,
-        uint8 _len
-    ) internal pure returns (uint8) {
-        require(_len <= 8, "invalid length");
-        require(((_start + _len) / 8) < _data.length, "out of bounds");
-
-        if (_len == 0) return 0;
-
-        bytes1 b1 = _data[_start / 8];
-        bytes1 b2 = _data[(_start + _len - 1) / 8];
-
-        uint16 frame = uint16((uint8(b1) << 8) + uint8(b2));
-
-        uint8 discard_upper = uint8(_start % 8);
-        uint8 discard_lower = 16 - discard_upper;
-
-        frame = frame << discard_upper;
-        frame = frame >> discard_lower;
-
-        return uint8(frame);
+    function _getQuadKey(
+        bytes memory _cellData,
+        uint256 _byteOffset,
+        uint8 _bitOffset
+    ) internal pure returns (uint8 quadKey) {
+        return
+            (uint8(_cellData[_bitOffset / 8 + _byteOffset]) >>
+                (6 - (_bitOffset % 8))) & 0x3;
     }
 
-    function _read64(
-        bytes memory _data,
-        uint256 _start,
-        uint8 _len
-    ) internal pure returns (uint64) {
-        require(_len <= 64, "invalid length");
-        require(((_start + _len) / 8) < _data.length, "out of bounds");
+    function registeredCells(bytes memory _parentCell)
+        public
+        view
+        returns (uint8 ret)
+    {
+        GeoCellNode storage currentNode = _getNode(_parentCell);
+        bool[4] memory cells = currentNode.cells;
+        return
+            (cells[0] ? 8 : 0) +
+            (cells[1] ? 4 : 0) +
+            (cells[2] ? 2 : 0) +
+            (cells[3] ? 1 : 0);
+    }
 
-        if (_len == 0) return 0;
+    function propertyOf(bytes memory _parentCell)
+        public
+        view
+        returns (uint256 propId)
+    {
+        GeoCellNode storage currentNode = _getNode(_parentCell);
+        return currentNode.propId;
+    }
 
-        uint72 frame = 0;
-        for (uint256 idx = _start / 8; idx <= (_start + _len - 1) / 8; idx++) {
-            frame = (frame << 8) | uint8(_data[idx]);
+    function _getNode(bytes memory _cell)
+        internal
+        view
+        returns (GeoCellNode storage node)
+    {
+        uint8 blockLen = uint8(_cell[0]);
+
+        uint8 faceId = (uint8(_cell[1]) >> 4) & 0x7;
+        GeoCellNode storage currentNode = _registeredLand[faceId];
+
+        for (uint8 bit = 4; bit < blockLen; bit += 2) {
+            uint8 quadKey = _getQuadKey(_cell, 1, bit);
+            currentNode = currentNode.children[quadKey];
         }
 
-        uint8 discard_upper = uint8(_start % 8);
-        uint8 discard_lower = 64 - discard_upper;
-
-        frame = frame << discard_upper;
-        frame = frame >> discard_lower;
-
-        return uint64(frame);
+        return currentNode;
     }
 }
--- blockchain/test/imovel_registry.js
+++ blockchain/test/imovel_registry.js
@@ -1,13 +1,14 @@
-//const ImovelRegistry = artifacts.require("ImovelRegistry");
+const truffleAssert = require('truffle-assertions');
+const ImovelRegistry = artifacts.require("ImovelRegistry");
 
 const URI1 = 'bafybeidi6mtwtmm6vrncytqiliiofyhxq5ys476dl7sdpwcfxmuphnus3i';
 
-//const TRANSFER_EVENT = web3.utils.soliditySha3('Transfer(address,address,uint256)');
+const TRANSFER_EVENT = web3.utils.soliditySha3('Transfer(address,address,uint256)');
 
 const REG = ["89c2598b57c", "89c2598b584", "89c2598b58c", "89c2598c97", "89c2598c99", "89c2598c9f", "89c2598ca4", "89c2598ca9", "89c2598caf", "89c2598cb04", "89c2598cbb", "89c2598cbd"];
 const REG_NEIGHBOR = ["89c2598b55", "89c2598cab", "89c2598cad", "89c2598cb27", "89c2598cb29", "89c259f3514", "89c259f351c", "89c259f353", "89c259f355", "89c259f3564", "89c259f356c", "89c259f4a94", "89c259f4a9c", "89c259f4ab"];
 
-const REG_LARGER = ["89c259"];
+const REG_LARGER = ["89c257", "89c259"];
 const REG_LARGER_NEIGHBOR = ["89c257"]
 
 const REG_SMALLER = ["89c2598ca85", "89c2598ca8f", "89c2598ca91", "89c2598ca9b"];
@@ -16,7 +17,7 @@ const REG_MULTIFACE = ["077ffffffc", "0780000004", "92d5555554", "932aaaaaac"];
 
 const REG_30 = ["89c2598ca846bd55", "89c2598ca846bd57", "89c2598ca846bd53"];
 
-/*
+
 contract("ImovelRegistry", (accounts) => {
     it("deve iniciar com saldo zerado", async () => {
         const instance = await ImovelRegistry.deployed();
@@ -27,7 +28,32 @@ contract("ImovelRegistry", (accounts) => {
     it("deve registrar um imóvel", async () => {
         const instance = await ImovelRegistry.deployed();
 
-        const claimTx = await instance.claimLand(accounts[0], URI1);
+        const claimTx = await instance.claimLand(accounts[0], URI1, pack_cell_data(REG));
+        const txReceipt = await web3.eth.getTransactionReceipt(claimTx.receipt.transactionHash);
+        const transferEvent = txReceipt.logs.find(log => log.topics[0] == TRANSFER_EVENT);
+        const tokenId = transferEvent.topics[3];
+
+        const balance = await instance.balanceOf.call(accounts[0]);
+        const owner = await instance.ownerOf.call(tokenId);
+        const tokenURI = await instance.tokenURI.call(tokenId);
+        const propertyOf = await instance.propertyOf.call(pack_cell(REG[0]));
+
+        assert.equal(balance, 1, "saldo da conta != 1 após mint");
+        assert.equal(owner, accounts[0], "token não transferido corretamente");
+        assert.equal(tokenURI, 'ipfs://' + URI1, "token URI incorreto");
+        assert.equal(BigInt(propertyOf), tokenId, "propriedade nao atribuida a celula");
+    });
+
+    it("deve preservar saldos de um teste para o outro", async () => {
+        const instance = await ImovelRegistry.deployed();
+        const balance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(balance, 1, "saldos não preservados");
+    });
+
+    it("deve registrar um imóvel vizinho", async () => {
+        const instance = await ImovelRegistry.deployed();
+
+        const claimTx = await instance.claimLand(accounts[0], URI1, pack_cell_data(REG_NEIGHBOR));
         const txReceipt = await web3.eth.getTransactionReceipt(claimTx.receipt.transactionHash);
         const transferEvent = txReceipt.logs.find(log => log.topics[0] == TRANSFER_EVENT);
         const tokenId = transferEvent.topics[3];
@@ -36,79 +62,128 @@ contract("ImovelRegistry", (accounts) => {
         const owner = await instance.ownerOf.call(tokenId);
         const tokenURI = await instance.tokenURI.call(tokenId);
 
-        assert.equal(balance, 1, "saldo da conta != 1 após mint");
+        assert.equal(balance, 2, "saldo da conta != 2 após mint");
         assert.equal(owner, accounts[0], "token não transferido corretamente");
         assert.equal(tokenURI, 'ipfs://' + URI1, "token URI incorreto");
     });
 
-    it("deve preservar saldos de um teste para o outro", async () => {
+    it("não deve registrar imovel cobrindo outro", async () => {
         const instance = await ImovelRegistry.deployed();
+
+        await truffleAssert.reverts(instance.claimLand(accounts[0], URI1, pack_cell_data(REG_LARGER)), "Registro sobrepoe area registrada");
+
         const balance = await instance.balanceOf.call(accounts[0]);
-        assert.equal(balance, 1, "saldos não preservados");
+        assert.equal(balance, 2, "saldo da conta != 2 após mint");
+    });
+
+    it("não deve registrar imovel dentro de outro", async () => {
+        const instance = await ImovelRegistry.deployed();
+
+        await truffleAssert.reverts(instance.claimLand(accounts[0], URI1, pack_cell_data(REG_SMALLER)), "Registro dentro de area ja registrada");
+
+        const balance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(balance, 2, "saldo da conta != 2 após mint");
     });
 });
-*/
 
-pack_cell_data(REG);
-pack_cell_data(REG_30);
-pack_cell_data(REG_MULTIFACE);
+
+
 
 function pack_cell_data(cells) {
-    // sort the arrays to find the common prefix, add to set to remove duplicates
+    // sort the arrays to find the common prefix, add to set to remove duplicates, bit-shift right to add 0 to msb
     const cellInts = [...new Set(cells.map(c => BigInt('0x' + c.padEnd(16, '0'))))].sort();
 
-    let prefixLen = 0;
+    let prefixLen = 0n;
     if (cellInts.length > 1) {
         const firstCell = cellInts[0];
         const lastCell = cellInts[cellInts.length - 1];
 
         if (firstCell >> 61n === lastCell >> 61n) {
-            prefixLen = 3
+            prefixLen = 3n;
 
             for (let idx = 59n; idx > 1n; idx = idx - 2n) {
                 if (firstCell >> idx !== lastCell >> idx) break;
-                prefixLen += 2;
+                prefixLen += 2n;
             }
         }
     }
 
     const output = [];
-    let pos = 0;
-
-    pos = _writeData(output, prefixLen, pos, 6);
 
     if (prefixLen > 0) {
-        const prefix = cellInts[0] >> (64n - BigInt(prefixLen))
-        console.log('prefix', prefix.toString(2));
-        pos = _writeData(output, prefix, pos, prefixLen);
+        prefixLen += 1n;
+        output.push(Number(prefixLen) & 0x00ff);
+
+        const prefix = cellInts[0] >> (65n - prefixLen)
+
+        for (let idx = 0n; idx < prefixLen; idx += 8n) {
+            const align = prefixLen - idx < 8n ? 8n - (prefixLen - idx) : 0n;
+            output.push(Number(((prefix >> (prefixLen - 8n - idx + align)) << align) & 0x00ffn));
+        }
+
+    } else {
+        output.push(0);
     }
 
-    const mask = (2n ** (64n - BigInt(prefixLen))) - 1n
+    const mask = (2n ** (65n - prefixLen)) - 1n
 
     for (idx in cellInts) {
         let cellNoPrefix = cellInts[idx] & mask;
 
         // trim lsb zeros
-        let trimmed = 0;
+        let trimmed = 0n;
         while ((cellNoPrefix & 3n) == 0n) {
             cellNoPrefix = cellNoPrefix >> 2n;
-            trimmed += 2;
+            trimmed += 2n;
         }
 
         // trim last bit
         cellNoPrefix = cellNoPrefix >> 1n;
 
-        const cellLen = 64 - prefixLen - trimmed - 1;
+        const cellLen = 65n - prefixLen - trimmed - 1n;
 
-        pos = _writeData(output, cellLen, pos, 6);
-        pos = _writeData(output, cellNoPrefix, pos, cellLen);
+        output.push(Number(cellLen) & 0x00ff);
 
-        console.log(cellNoPrefix.toString(2), 'length', cellLen);
+        for (let idx = 0n; idx < cellLen; idx += 8n) {
+            const align = cellLen - idx < 8n ? 8n - (cellLen - idx) : 0n;
+            output.push(Number(((cellNoPrefix >> (cellLen - 8n - idx + align)) << align) & 0x00ffn));
+        }
     }
 
-    console.log(cellInts.map(c => c.toString(2).slice(0, prefixLen) + " " + c.toString(2).slice(prefixLen)));
+    return '0x' + toHexString(output);
 }
 
-function _writeData(output, data, start, lenBits) {
+function pack_cell(cell) {
+    // sort the arrays to find the common prefix, add to set to remove duplicates, bit-shift right to add 0 to msb
+    let cellInt = BigInt('0x' + cell.padEnd(16, '0'));
 
+    // trim lsb zeros
+    let trimmed = 0n;
+    while ((cellInt & 3n) == 0n) {
+        cellInt = cellInt >> 2n;
+        trimmed += 2n;
+    }
+
+    // trim last bit
+    cellInt = cellInt >> 1n;
+
+    const cellLen = 65n - trimmed - 1n;
+
+    const output = [];
+
+    output.push(Number(cellLen) & 0x00ff);
+
+    for (let idx = 0n; idx < cellLen; idx += 8n) {
+        const align = cellLen - idx < 8n ? 8n - (cellLen - idx) : 0n;
+        output.push(Number(((cellInt >> (cellLen - 8n - idx + align)) << align) & 0x00ffn));
+    }
+
+    return '0x' + toHexString(output);
 }
+
+function toHexString(byteArray) {
+    return Array.from(byteArray, function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+}
+
